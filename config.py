6dccca878fbb8c718074e2692a278a616faa2edf
UGOCHUKWU EZIGBO <92388596+Ugogabby@users.noreply.github.com>
2024-03-18 06:25:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,4 @@ import sqlite3
 def connect_to_db():
   conn = sqlite3.connect("participant_data.db") 
   cursor = conn.cursor()
-=======
-import sqlite3
-
-def connect_to_db():
-  conn = sqlite3.connect("participant_data.db") 
-  cursor = conn.cursor()
->>>>>>> c07242953e66244744d7a96d07be080b640b84ab
   return conn, cursor
